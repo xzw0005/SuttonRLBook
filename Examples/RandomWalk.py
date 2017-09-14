afc8cbaaf316d415622b971e05f76068a529262b
wangxing <wangxing@XPS9550>
2017-09-14 02:33:12 -0500

    errors list has problems

--- Examples/RandomWalk.py
+++ Examples/RandomWalk.py
@@ -9,12 +9,17 @@ import matplotlib.pyplot as plt
 
 numStates = 7
 startState = numStates/2 +1
-states = np.arange(1, numStates+1)
-absorbingState = [0, numStates+1]
+states = np.arange(1, numStates-1)
+print states
+absorbingState = [0, numStates-1]
 
 actionLeft = 0
 actionRight = 1
 
+trueValues = np.zeros(numStates)
+trueValues[1:numStates-1] = np.arange(1, numStates-1) / float(numStates-1)
+trueValues[numStates-1] = 1
+print trueValues[1:-1]
     
 class TDlambda(object):
     '''
@@ -27,17 +32,18 @@ class TDlambda(object):
         self.lamb = lamb 
         self.alpha = alpha 
         self.gamma = gamma
-        self.values = np.zeros(numStates + 2)
+        self.values = np.zeros(numStates)
         self.newEpisode()
         
     def newEpisode(self):
-        self.eligibility = np.zeros(numStates + 2)
+        self.eligibility = np.zeros(numStates)
         self.lastState = startState
         self.stateValue = 0.0
         
     def learn(self, state, reward):
         self.eligibility *= (self.lamb * self.gamma)
-        delta = reward + self.value[state] * self.gamma - self.values[self.lastState]
+        self.eligibility[self.lastState] += 1
+        delta = reward + self.values[state] * self.gamma - self.values[self.lastState]
         delta *= self.alpha
         self.values += delta * self.eligibility
         self.lastState = state
@@ -54,7 +60,7 @@ def randomWalk(valueFunction):
             newState = currentState + 1
         if newState == 0:
             reward = -1
-        elif newState == numStates + 1:
+        elif newState == numStates - 1:
             reward = 1
         else:
             reward = 0
@@ -67,11 +73,24 @@ def rmsError(lambdas, alphas, episodes=10, runs=100):
         for lambIndex, lamb in zip(range(len(lambdas)), lambdas):
             for alphaIndex, alpha in zip(range(len(alphas)), alphas):
                 instance = TDlambda(lamb, alpha)
-                for episode in episodes:
+                for episode in range(episodes):
                     randomWalk(instance)
-                    stateValues = instance.values
-                    errors[lambIndex][alphaIndex] += np.sqrt(np.mean(np.power(stateValues - idealPredictions)))
+                    print instance.values
+                    stateValues = [instance.values[s] for s in states]
+                    errors[lambIndex] += np.sqrt(np.mean(np.power(stateValues - trueValues[1:-1], 2)))
+    for err in errors:
+        err /= episodes * runs 
+#    print len(errors)
+    plt.figure()
+    for i in range(len(lambdas)):
+        plt.plot(alphas[i], errors[i], '-o', label=r'$\lambda=$'+str(lambdas[i]))
+    plt.xlabel(r'$\alpha$')
+    plt.ylabel('RMS error')
+    plt.show()
         
 def figure4():
     lambdas = [0, .1, .3, .5, .7, .9, 1]
     alphas = np.arange(0, .7, .1)
+    rmsError(lambdas, alphas)
+    
+figure4()
